52fec63503d8127d82f202a934ed0d4a7542f179
Jessica <jchung722@gmail.com>
2016-12-13 13:40:45 -0800

    wincheck function added

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -7,10 +7,24 @@ describe('Game', function() {
     testGame = new Game("elle", "jessica");
   });
 
-  describe('get name and letter for player object in game', function() {
+  describe('Game', function() {
     it('should return player name and letter', function() {
       expect(testGame.player1.name).toEqual("ELLE");
       expect(testGame.player2.name).toEqual("JESSICA");
+    });
+  });
+
+  describe('winCheck', function() {
+    it('should return true if there is a winner', function() {
+      expect(testGame.winCheck(["X", "X", "X", " ", " ", " ", " ", " ", " "]));
+    });
+
+    it('should return false with an empty board', function() {
+      expect(testGame.winCheck([" ", " ", " ", " ", " ", " ", " ", " ", " "]));
+    });
+
+    it('should return false when the board is filled but there is no winner', function() {
+      expect(testGame.winCheck(["X", "X", "O", "O", "O", "X", "X", "O", "X"]));
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -11,10 +11,29 @@ var Game = function(player1, player2) {
 
   // player1.letter for move
 
-
+  var winCheck = function(board) {
+    if (board[0] == board[1] == board[2] && board[0] != " ") {
+      return true;
+    } else if (board[3] == board[4] == board[5] && board[3] != " ") {
+      return true;
+    } else if (board[6] == board[7] == board[8] && board[6] != " ") {
+      return true;
+    } else if (board[0] == board[3] == board[6] && board[0] != " ") {
+      return true;
+    } else if (board[1] == board[4] == board[7] && board[1] != " ") {
+      return true;
+    } else if (board[2] == board[5] == board[8] && board[2] != " ") {
+      return true;
+    } else if (board[2] == board[4] == board[6] && board[2] != " ") {
+      return true;
+    } else if (board[0] == board[4] == board[8] && board[0] != " ") {
+      return true;
+    } else {
+      return false;
+    }
   };
 
 
-}
+  };
 
 module.exports = Game;
