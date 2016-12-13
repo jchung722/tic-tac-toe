edabea2aef4fa78cbef9918f37e3dbf6d9132ef7
Elle Vargas <vargas.elle@gmail.com>
2016-12-13 13:09:23 -0800

    testing

--- src/game.js
+++ src/game.js
@@ -1,13 +1,18 @@
+import _ from 'underscore';
 import Player from "player";
 
 var Game = function(player1, player2) {
 
-  const GAME_TILES = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  const GAME_TILES = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  var currentBoard = GAME_TILES;
 
   this.player1 = new Player(player1, "X");
   this.player2 = new Player(player2, "O");
 
   // player1.letter for move
+
+
+  };
 
 
 }
